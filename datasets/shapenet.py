3f45748d83fe0b2394818ebe8e97042ec607b2b0
Ivan Chukhran <ivan.chuhran@gmail.com>
2024-04-18 12:22:51 +0300

    add graph edge construction methods to dataset

--- datasets/shapenet.py
+++ datasets/shapenet.py
@@ -2,6 +2,9 @@ import os
 from typing import Callable
 import torch
 from torch import nn
+
+from torch_cluster import knn_graph, radius_graph
+
 from utils import load_ply
 from utils.transformations import random_rotation_matrix
 from .base_dataset import BaseDataset
@@ -14,10 +17,14 @@ class ShapeNet(BaseDataset):
                  classes: list | None = None,
                  use_random_rotation: bool = False,
                  num_samples: int = 4,
-                 transform: Callable | None = None):
+                 transform: Callable | None = None,
+                 edge_linkage: str = 'knn',
+                 edge_criteria: int | float = 9):
         super().__init__(dir_path, split, classes, transform)
         self.use_random_rotation = use_random_rotation
         self.num_samples = num_samples
+        self.edge_linkage = edge_linkage
+        self.edge_criteria = edge_criteria
         if not self.classes:
             self.classes = os.listdir(self.dir_path)
         split_file = os.path.join(self.dir_path, 'slices', 'splits', f"{split}.list")
@@ -29,7 +36,6 @@ class ShapeNet(BaseDataset):
         return len(self.filenames) * self.num_samples
 
     def __getitem__(self, idx: int):
-        print(idx)
         filename = self.filenames[idx // self.num_samples]
         text, format = filename.split('.')
         existing_filename = os.path.join(self.dir_path, 'slices', 'existing', f'{text}_{idx % self.num_samples}.{format}')
@@ -38,6 +44,7 @@ class ShapeNet(BaseDataset):
         missing, _, _ = load_ply(missing_filename)
         existing = torch.from_numpy(existing)
         missing = torch.from_numpy(missing)
+        edges = self.edge_linkage(existing, self.edge_criteria)
         if self.use_random_rotation:
             rotation_matrix = random_rotation_matrix()
             existing = torch.matmul(existing, rotation_matrix)
@@ -45,7 +52,4 @@ class ShapeNet(BaseDataset):
         if self.transform is not None:
             existing = self.transform(existing)
             missing = self.transform(missing)
-        return {
-            "existing": existing,
-            "missing": missing
-        }
+        return existing, edges, missing
